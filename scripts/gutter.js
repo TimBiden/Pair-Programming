80c48e63d08c8c862eded8f245ae0c2c0c250a13
TimBiden <timbiden@gmail.com>
2017-03-16 15:16:12 -0700

    Formatting.

--- scripts/gutter.js
+++ scripts/gutter.js
@@ -1,12 +1,16 @@
+/**
+ * Gets the number of lines from .textBox
+ * @param {element} element - The title of the book.
+ * @returns {Integer} Current number of lines in .textBox
+ */
 function getLength(element) {
   let linesTotal = 0;
   linesTotal = element.querySelectorAll('div').length + 1;
   console.log(linesTotal);
   return linesTotal;
-};
-
+}
 
 const box = document.querySelector('.textBox');
-box.addEventListener("keyup", function(){
+box.addEventListener('keyup', function () {
   getLength(box);
 });
