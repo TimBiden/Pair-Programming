3fde936b1dffbaef28c99f772d2ed7c927f4c5e6
TimBiden <timbiden@gmail.com>
2017-03-17 10:08:46 -0700

    Prevent updating after every keystroke.

--- scripts/gutter.js
+++ scripts/gutter.js
@@ -1,3 +1,5 @@
+let totalLines = 1;
+
 const lineNumbers = function lineNumbers(lines) {
   let allLines = '';
   for (let i = 1; i < lines; i += 1) {
@@ -6,14 +8,20 @@ const lineNumbers = function lineNumbers(lines) {
   console.log(allLines);
 };
 
+let unEqual = function unEqual(linesTotal) {
+  if (linesTotal !== totalLines){
+    totalLines = linesTotal;
+    lineNumbers(totalLines);
+  }
+}
+
 const getLength = function getLength(element) {
   const linesTotal = element.querySelectorAll('div').length + 1;
-  // console.log('getLength = ');
-  // console.log(linesTotal);
-  lineNumbers(linesTotal);
+  unEqual(linesTotal);
+  // lineNumbers(linesTotal);
 };
 
 const box = document.querySelector('.textBox');
-box.addEventListener('keyup', function() {
+box.addEventListener('keyup', function () {
   getLength(box);
 });
