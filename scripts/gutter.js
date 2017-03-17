dd5ca948ba410b569cb3201acef7e6f426f77dae
TimBiden <timbiden@gmail.com>
2017-03-17 10:48:51 -0700

    Line numbers updated.
    Craptastic formatting.

--- scripts/gutter.js
+++ scripts/gutter.js
@@ -1,14 +1,20 @@
 let totalLines = 1;
 
+function updateGutter(allLines) {
+  // console.log(allLines);
+  const element = document.getElementsByClassName('gutter')[0];
+  element.innerHTML = allLines;
+}
+
 const lineNumbers = function lineNumbers(lines) {
   let allLines = '';
   for (let i = 1; i < lines; i += 1) {
     allLines = `${allLines}\n ${i}.`;
   }
-  console.log(allLines);
+  updateGutter(allLines);
 };
 
-let unEqual = function unEqual(linesTotal) {
+function unEqual(linesTotal) {
   if (linesTotal !== totalLines){
     totalLines = linesTotal;
     lineNumbers(totalLines);
@@ -18,7 +24,6 @@ let unEqual = function unEqual(linesTotal) {
 const getLength = function getLength(element) {
   const linesTotal = element.querySelectorAll('div').length + 1;
   unEqual(linesTotal);
-  // lineNumbers(linesTotal);
 };
 
 const box = document.querySelector('.textBox');
