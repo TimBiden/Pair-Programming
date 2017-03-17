9e189366114a1a107ac64a6238ee98954ea4bd79
TimBiden <timbiden@gmail.com>
2017-03-17 14:01:09 -0700

    Add comments. Bad gutter formatting.

--- scripts/gutter.js
+++ scripts/gutter.js
@@ -1,32 +1,53 @@
 let totalLines = 1;
 
+//
+// Update the gutter with correct # of lines.
+//
 function updateGutter(allLines) {
   // console.log(allLines);
-  const element = document.getElementsByClassName('gutter')[0];
-  element.innerHTML = allLines;
+
+  for (let i = 0; i < allLines.length; i += 1) {
+    document.getElementsByClassName('gutter').innerHTML = ' ';
+    const temp = document.createElement('div');
+    temp.innerHTML = allLines[i];
+    document.getElementsByClassName('gutter')[0].appendChild(temp);
+  }
 }
 
+//
+// Create array of numbers and `.` to make up gutter.
+//
 const lineNumbers = function lineNumbers(lines) {
-  let allLines = '';
+  let allLines = [];
   for (let i = 1; i < lines; i += 1) {
-    allLines = `${allLines}\n ${i}.`;
+    allLines.push(`${i}.`);
   }
   updateGutter(allLines);
 };
 
+//
+// If the current # of lines = previous # of lines, do nothing.
+// Else, run lineNumbers.
+//
 function unEqual(linesTotal) {
-  if (linesTotal !== totalLines){
+  if (linesTotal !== totalLines) {
     totalLines = linesTotal;
     lineNumbers(totalLines);
   }
 }
 
+//
+// Get the length of the text box.
+//
 const getLength = function getLength(element) {
-  const linesTotal = element.querySelectorAll('div').length + 1;
+  const linesTotal = element.querySelectorAll('div').length + 2;
   unEqual(linesTotal);
 };
 
+//
+// Listen for Keyup to run getLength function.
+//
 const box = document.querySelector('.textBox');
-box.addEventListener('keyup', function () {
+box.addEventListener('keyup', function() {
   getLength(box);
 });
