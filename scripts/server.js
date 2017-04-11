08d48c2ed62187a1a09336a3697d5286f4fcc19a
TimBiden <timbiden@gmail.com>
2017-04-11 08:02:03 -0700

    Formatting

--- scripts/server.js
+++ scripts/server.js
@@ -1,8 +1,11 @@
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({port: 8080});
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
+const wss = new WebSocket.Server({
+  port: 8080,
+});
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
     console.log('received: %s', message);
   });
 
