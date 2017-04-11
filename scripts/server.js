1d70a917c1d1b50c6a24bc88a439c3f2cc9864cb
TimBiden <timbiden@gmail.com>
2017-04-11 12:44:53 -0700

    Combine server.js & app.js files.

--- scripts/server.js
+++ scripts/server.js
@@ -11,3 +11,14 @@ wss.on('connection', (ws) => {
 
   ws.send('something');
 });
+
+window.onload = function() {
+  // Get references to elements on page.
+  const form = document.getElementById('message-form');
+  const messageField = document.getElementById('message');
+  const messagesList = document.getElementById('messages');
+  const socketStatus = document.getElementById('status');
+  const closeButton = document.getElementById('close');
+
+  // Save for later code
+}
