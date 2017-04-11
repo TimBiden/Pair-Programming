cdab3f82543ed61534a5d4389d003faad35867b9
TimBiden <timbiden@gmail.com>
2017-04-11 12:07:11 -0700

    Create app.js

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  // Get references to elements on page.
+  const form = document.getElementByID('message-form');
+  const messageField = document.getElementByID('message');
+  const messagesList = document.getElementByID('messages');
+  const socketStatus = document.getElementByID('status');
+  const closeButton = document.getElementByID('close');
+
+  // Save for later code
+}
