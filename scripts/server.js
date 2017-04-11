f9c9ff87ae5575fb29f201c1b050e5f4cf6180c3
TimBiden <timbiden@gmail.com>
2017-04-11 13:22:43 -0700

    Try to get demo working.
    Not yet.
    Going further down the rabbit hole.

--- scripts/server.js
+++ scripts/server.js
@@ -1,16 +1,19 @@
-const WebSocket = require('ws');
-
-const wss = new WebSocket.Server({
-  port: 8080,
-});
-
-wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    console.log('received: %s', message);
-  });
-
-  ws.send('something');
-});
+//
+// Original server code from https://github.com/websockets/ws#server-example
+//
+// const WebSocket = require('ws');
+//
+// const wss = new WebSocket.Server({
+//   port: 8080,
+// });
+//
+// wss.on('connection', (ws) => {
+//   ws.on('message', (message) => {
+//     console.log('received: %s', message);
+//   });
+//
+//   ws.send('something');
+// });
 
 window.onload = function() {
   // Get references to elements on page.
@@ -20,5 +23,12 @@ window.onload = function() {
   const socketStatus = document.getElementById('status');
   const closeButton = document.getElementById('close');
 
-  // Save for later code
+  // Create new WebSocket
+  const socket = new WebSocket('ws://echo.websocket.org');
+
+  // Show "Connected." message when websocket opened successfully.
+  socket.onopen = function(event) {
+    socketStatus.innerHTML = 'Connected to: ' + event.currentTarget.URL;
+    socketStatus.classname = 'open';
+  };
 }
