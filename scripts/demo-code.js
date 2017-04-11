3f5b51aa6e3bdb369575d27af3b8317fcd2c26a6
TimBiden <timbiden@gmail.com>
2017-04-11 15:55:46 -0700

    Segregate WebSocket JS files.

---
+++ scripts/demo-code.js
@@ -0,0 +1,66 @@
+//
+// Code from http://blog.teamtreehouse.com/an-introduction-to-websockets
+//
+window.onload = function() {
+  // Get references to elements on page.
+  const form = document.getElementById('message-form');
+  const messageField = document.getElementById('message');
+  const messagesList = document.getElementById('messages');
+  const socketStatus = document.getElementById('status');
+  const closeButton = document.getElementById('close');
+
+  // Create new WebSocket
+  const socket = new WebSocket('ws://echo.websocket.org');
+
+  // Show "Connected." message when websocket opened successfully.
+  socket.onopen = function(event) {
+    socketStatus.innerHTML = `Connected to: ${event.currentTarget.URL}`;
+    socketStatus.classname = 'open';
+  };
+
+  // Error Handling
+  socket.onerror = function(error) {
+    console.log(`WebSocket error: ${error}`);
+  };
+
+  // Send a message.
+  form.onsubmit = function(e) {
+    e.preventDefault();
+
+    // retrieve message from textarea
+    const message = messageField.value;
+
+    // Send message through websocket
+    socket.send(message);
+
+    // add message to messagesList
+    messagesList.innerHTML += `<li class="sent"><span>Sent:</span>${message}</li>`;
+
+    // clear out message field
+    messageField.value = '';
+
+    return false;
+  };
+
+  // Receive messages
+  socket.onmessage = function(event) {
+    const message = event.data;
+    messagesList.innerHTML += `<li class="received"><span>Received:</span> ${message} </li>`
+  };
+
+  // Closing connections
+  closeButton.onclick = function(e) {
+    e.preventDefault();
+
+    // Close the WebSocket
+    socket.close();
+
+    return false;
+  };
+
+  // Closing Message
+  socket.onclose = function(event) {
+    socketStatus.innerHTML = 'Disconnected from WebSocket server.';
+    socketStatus.classname = 'closed';
+  };
+};
--- scripts/server.js
+++ scripts/server.js
@@ -1,80 +1,16 @@
 //
-// Original server code from https://github.com/websockets/ws#server-example
+// WS Server code.
 //
-// const WebSocket = require('ws');
-//
-// const wss = new WebSocket.Server({
-//   port: 8080,
-// });
-//
-// wss.on('connection', (ws) => {
-//   ws.on('message', (message) => {
-//     console.log('received: %s', message);
-//   });
-//
-//   ws.send('something');
-// });
+const WebSocket = require('ws');
 
-window.onload = function() {
-  // Get references to elements on page.
-  const form = document.getElementById('message-form');
-  const messageField = document.getElementById('message');
-  const messagesList = document.getElementById('messages');
-  const socketStatus = document.getElementById('status');
-  const closeButton = document.getElementById('close');
+const wss = new WebSocket.Server({
+  port: 8080,
+});
 
-  // Create new WebSocket
-  const socket = new WebSocket('ws://echo.websocket.org');
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    console.log('received: %s', message);
+  });
 
-  // Show "Connected." message when websocket opened successfully.
-  socket.onopen = function(event) {
-    socketStatus.innerHTML = `Connected to: ${event.currentTarget.URL}`;
-    socketStatus.classname = 'open';
-  };
-
-  // Error Handling
-  socket.onerror = function(error) {
-    console.log(`WebSocket error: ${error}`);
-  };
-
-  // Send a message.
-  form.onsubmit = function(e) {
-    e.preventDefault();
-
-    // retrieve message from textarea
-    const message = messageField.value;
-
-    // Send message through websocket
-    socket.send(message);
-
-    // add message to messagesList
-    messagesList.innerHTML += `<li class="sent"><span>Sent:</span>${message}</li>`;
-
-    // clear out message field
-    messageField.value = '';
-
-    return false;
-  };
-
-  // Receive messages
-  socket.onmessage = function(event) {
-    const message = event.data;
-    messagesList.innerHTML += `<li class="received"><span>Received:</span> ${message} </li>`
-  };
-
-  // Closing connections
-  closeButton.onclick = function(e) {
-    e.preventDefault();
-
-    // Close the WebSocket
-    socket.close();
-
-    return false;
-  };
-
-  // Closing Message
-  socket.onclose = function(event) {
-    socketStatus.innerHTML = 'Disconnected from WebSocket server.';
-    socketStatus.classname = 'closed';
-  };
-};
+  ws.send('something');
+});
