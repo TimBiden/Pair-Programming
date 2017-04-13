4cd768e4f65c2a17e99eabfcb7b7135ac52dd9b1
TimBiden <timbiden@gmail.com>
2017-04-13 14:58:48 -0700

    Not auto receiving except on reload.

--- scripts/server.js
+++ scripts/server.js
@@ -1,15 +1,30 @@
-//
 // WS Server code.
-//
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({
   port: 8080,
 });
 
+const messages = [
+  'You\'re connected.',
+  'Now get to work!',
+];
+
 wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    console.log(`received: ${message}`);
-    ws.send(`Received your message of "${message}".`);
+  // Sends entire history to all new session participants
+  for (let message of messages) {
+    ws.send(message);
+  }
+
+  ws.on('message', (data) => {
+    // Capture data sent to server
+    messages.push(data);
+
+    // Broascast to everyone else... Not original sender
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
   });
 });
