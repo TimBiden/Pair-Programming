ad3550e34a0aaa32fa409efe1a27918cdb779ad5
TimBiden <timbiden@gmail.com>
2017-04-19 12:10:12 -0700

    Update .gitignore

---
+++ scripts/server-old.js
@@ -0,0 +1,27 @@
+// WS Server code.
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({
+  port: 8080,
+});
+
+const messages = ['Enter your code here...'];
+
+wss.on('connection', (ws) => {
+  // Send the existing message history to all new connections that join.
+  for (const message of messages) {
+    ws.send(message);
+  }
+
+  ws.on('message', (data) => {
+    // Capture the data we received.
+    messages.push(data);
+
+    // Broadcast to everyone else.
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
