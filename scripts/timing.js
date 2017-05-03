0d7e878f43c3310d919f1aae00102abfe7cc0e2a
TimBiden <timbiden@gmail.com>
2017-05-02 18:02:35 -0700

    Create block & unblock functions

--- scripts/timing.js
+++ scripts/timing.js
@@ -1,15 +1,33 @@
 let timer;
+let toBlockOrNot;
+
+/**
+ * Unblock text editor for recipient
+ * @returns {void}
+ */
+function unBlock() {
+  console.log('You can type now');
+  toBlockOrNot = false;
+}
+
+/**
+ * Unblock text editor for recipient
+ * @returns {void}
+ */
+function reBlock() {
+  console.log('You are blocked now');
+  toBlockOrNot = true;
+}
 
 /**
  * Check time since other user updated document.
  * If > 1.5 seconds, release textarea for editing.
- * @param {number} arg total number of lines in the editor.
  * @returns {void}
  */
 function checkTime() {
   clearTimeout(timer);
+  reBlock();
   timer = setTimeout(() => {
-    console.log('Time\'s up.');
-    // validate(arg);
+    unBlock();
   }, 1500);
 }
