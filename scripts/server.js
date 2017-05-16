77cac627371550484b6b9beca9091896bfc0861e
TimBiden <timbiden@gmail.com>
2017-05-16 11:52:30 -0700

    Update for broken server

--- scripts/server.js
+++ scripts/server.js
@@ -2,47 +2,47 @@
 
 // Requirements
 const WebSocket = require('ws');
-// const mongoose = require('mongoose');
-// const configReq = require('../scripts/config.js');
-//
-// // Variables
-// const messages = ['Enter your code here...'];
-// const dbConfig = process.env.DATABASE_URI
-//
-// // Database connection
-// const PORT = process.env.PORT || 5000;
-// mongoose.connect(dbConfig);
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', () => {
-//   console.log('We\'re connected. I think.');
-// });
-//
-// // Define Mongo schema
-// const editorSchema = mongoose.Schema({
-//   session: String,
-//   codeBox: String,
-// });
-//
-// let Editor = mongoose.model('Editor', editorSchema);
-// // Delete after configuring session IDs
-// const sessionID = PORT;
-// const textareaToDB = 'Yada, yada, freaking yada...';
-// // End deletion after configuring session IDs
-//
-// let editorInstance = new Editor({
-//   session: sessionID,
-//   codeBox: textareaToDB,
-// });
-//
-// function onEditorSave(error, model) {
-//   if (error) {
-//     return console.error(error);
-//   }
-//   console.log(model);
-// }
-//
-// editorInstance.save(onEditorSave);
+const mongoose = require('mongoose');
+const configOverride = require('../scripts/config.js');
+
+// Variables
+const messages = ['Enter your code here...'];
+const dbConfig = process.env.MONGODB_URI || configOverride.MONGODB_URI;
+
+// Database connection
+const PORT = process.env.PORT || 5000;
+mongoose.connect(dbConfig);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('We\'re connected. I think.');
+});
+
+// Define Mongo schema
+const editorSchema = mongoose.Schema({
+  session: String,
+  codeBox: String,
+});
+
+let Editor = mongoose.model('Editor', editorSchema);
+// Delete after configuring session IDs
+const sessionID = PORT;
+const textareaToDB = 'Yada, yada, freaking yada...';
+// End deletion after configuring session IDs
+
+let editorInstance = new Editor({
+  session: sessionID,
+  codeBox: textareaToDB,
+});
+
+function onEditorSave(error, model) {
+  if (error) {
+    return console.error(error);
+  }
+  console.log(model);
+}
+
+editorInstance.save(onEditorSave);
 
 // WebSocket connection
 const wss = new WebSocket.Server({
