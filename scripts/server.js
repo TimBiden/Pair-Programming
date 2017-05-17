e9ee9380a2b4bd7d840980b5e213b24228cf553e
TimBiden <timbiden@gmail.com>
2017-05-17 09:37:40 -0700

    Uncomment most of the code

--- scripts/server.js
+++ scripts/server.js
@@ -8,43 +8,43 @@ const mongoose = require('mongoose');
 // Variables
 // Set WS port
 const PORT = process.env.PORT || 5000;
-// Set Web Server Variables
-// const messages = ['Enter your code here...'];
-// const dbConfig = process.env.DATABASE_URI
+Set Web Server Variables
+const messages = ['Enter your code here...'];
+const dbConfig = process.env.DATABASE_URI
 
-// // Database connection
-// mongoose.connect(dbConfig);
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', () => {
-//   console.log('We\'re connected. I think.');
-// });
-//
-// // Define Mongo schema
-// const editorSchema = mongoose.Schema({
-//   session: String,
-//   codeBox: String,
-// });
-//
-// let Editor = mongoose.model('Editor', editorSchema);
-// // Delete after configuring session IDs
-// const sessionID = PORT;
-// const textareaToDB = 'Yada, yada, freaking yada...';
-// // End deletion after configuring session IDs
-//
-// let editorInstance = new Editor({
-//   session: sessionID,
-//   codeBox: textareaToDB,
-// });
-//
-// function onEditorSave(error, model) {
-//   if (error) {
-//     return console.error(error);
-//   }
-//   console.log(model);
-// }
-//
-// editorInstance.save(onEditorSave);
+// Database connection
+mongoose.connect(dbConfig);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('We\'re connected. I think.');
+});
+
+// Define Mongo schema
+const editorSchema = mongoose.Schema({
+  session: String,
+  codeBox: String,
+});
+
+let Editor = mongoose.model('Editor', editorSchema);
+// Delete after configuring session IDs
+const sessionID = PORT;
+const textareaToDB = 'Yada, yada, freaking yada...';
+// End deletion after configuring session IDs
+
+let editorInstance = new Editor({
+  session: sessionID,
+  codeBox: textareaToDB,
+});
+
+function onEditorSave(error, model) {
+  if (error) {
+    return console.error(error);
+  }
+  console.log(model);
+}
+
+editorInstance.save(onEditorSave);
 
 // WebSocket connection
 const wss = new WebSocket.Server({
