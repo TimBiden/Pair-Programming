ed3dea1b8a453957fd5002c9afb89d561c137608
TimBiden <timbiden@gmail.com>
2017-05-30 09:04:11 -0700

    Revert "Remove Node code to parse URL."
    This reverts commit d3c101cefd674f0369db9dadb86916d8dc651a96.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@
 // Requirements
 const WebSocket = require('ws');
 const mongoose = require('mongoose');
+const http = require('http');
 const sessionFile = require('./session.js');
 const configFile = require('./config.js');
 
@@ -21,6 +22,18 @@ if (PORT === 5000) {
 } else {
   dbConfig = process.env.MONGODB_URI;
 }
+
+// Create HTTP Server
+const server = http.createServer((req, res) => {
+  // This doesn't run.
+  console.log(`req = ${req}`);
+  console.log(`The URL reqed is ${req.url}.`);
+  console.log(`res = ${res}`);
+});
+
+server.listen(PORT, () => {
+  console.log(`The server is listening on port ${PORT}`);
+});
 
 // Database connection
 mongoose.connect(dbConfig);
