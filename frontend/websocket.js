5e46b785d184356e7ad8013e4baa622cafbac6d1
TimBiden <timbiden@gmail.com>
2017-06-20 13:02:14 -0700

    Get ws port number via fetch. Test 1.

--- frontend/websocket.js
+++ frontend/websocket.js
@@ -1,53 +1,61 @@
-// Get references to elements on page.
-const messageField = document.getElementById('mainTextArea');
-
-//
-// Create new WebSocket on Heroku
-//
-// const server = 'ws://sleepy-ridge-88571.herokuapp.com';
-// const server = 'wss://sleepy-ridge-88571.herokuapp.com';
-const server = 'ws://sleepy-ridge-88571.herokuapp.com:5299';
-// const server = 'wss://sleepy-ridge-88571.herokuapp.com:8080';
-// Local config file
-// const server = 'ws://localhost:8080';
-
-const socket = new WebSocket(server);
+fetch('/ws-port').then((response) => {
+  console.log(`Response is: ${response}`);
+  // connect(response);
+});
 
 /**
- * Retrieve data from textarea
- * Send message data through websocket
+ * Connect via websocket
+ * @param {number} response Current ws port number
  * @returns {void}
  */
-function sendData() {
-  // retrieve data from textarea
-  const message = messageField.value;
+function connect(response) {
+  // Get references to elements on page.
+  const messageField = document.getElementById('mainTextArea');
 
-  // Send message data through websocket
-  socket.send(message);
-}
+  //
+  // Create new WebSocket on Heroku
+  //
+  const server = `ws://sleepy-ridge-88571.herokuapp.com:${response}`;
+  // const server = 'ws://localhost:8080';
 
-/**
- * Send and receive message data through websocket
- * @returns {void}
- */
-window.onload = () => {
-  // Receive messages
-  socket.onmessage = (event) => {
-    const message = event.data;
+  const socket = new WebSocket(server);
 
-    // Print message value to all textarea boxes in session
-    document.getElementById('mainTextArea').value = message;
-    resizeTextBox();
+  /**
+   * Retrieve data from textarea
+   * Send message data through websocket
+   * @returns {void}
+   */
+  function sendData() {
+    // retrieve data from textarea
+    const message = messageField.value;
+
+    // Send message data through websocket
+    socket.send(message);
+  }
+
+  /**
+   * Send and receive message data through websocket
+   * @returns {void}
+   */
+  window.onload = () => {
+    // Receive messages
+    socket.onmessage = (event) => {
+      const message = event.data;
+
+      // Print message value to all textarea boxes in session
+      document.getElementById('mainTextArea').value = message;
+      resizeTextBox();
+      getLines();
+      checkTime();
+    };
+
+    // Making magic happen!!!
+    messageField.addEventListener('keyup', () => {
+      sendData();
+      getLines();
+    });
+
+    // Run numbering function
     getLines();
-    checkTime();
   };
-
-  // Making magic happen!!!
-  messageField.addEventListener('keyup', () => {
-    sendData();
-    getLines();
-  });
-
-  // Run numbering function
-  getLines();
-};
+}
