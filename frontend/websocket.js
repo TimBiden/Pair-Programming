3028c621d8ed4a9ad8482a8b685e7f0f01c3a033
TimBiden <timbiden@gmail.com>
2017-06-20 13:11:21 -0700

    response.json

--- frontend/websocket.js
+++ frontend/websocket.js
@@ -1,6 +1,8 @@
 fetch('/ws-port').then((response) => {
   console.log(`Response is: ${response.status}`);
-  // connect(response);
+  response.json().then((data) => {
+    console.log(data);
+  });
 });
 
 /**
