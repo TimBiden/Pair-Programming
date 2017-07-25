58fa7086b711e89b939d8a8c33eff2b7c341eaae
TimBiden <timbiden@gmail.com>
2017-07-25 14:52:45 -0700

    Mike's code.

--- backend/server.js
+++ backend/server.js
@@ -266,9 +266,15 @@ wss.on('connection', (ws) => {
 
   // Send the existing message history to all new connections that join.
 
+  let response = {};
+
   if (sessionID) {
     console.log('there are multiple messages');
     messages = ['Enter your code here...', sessionID];
+    response.type = 'SESSION_ID';
+    response.data = {
+      sessionID
+    };
   } else {
     messages = ['Enter your code here...'];
   }
@@ -277,26 +283,32 @@ wss.on('connection', (ws) => {
     ws.send(textBackToEditor);
     // messages = ['Enter your code here...'];
   } else {
-    for (const message of messages) {
-      ws.send(message);
-    }
-  }
-
-  ws.on('message', (data) => {
-    // Capture the data we received.
-    // messages.push(data);
-    // for (let wsc of clientPool[sessionID]) {
-    //   wsc.send(data);
+    ws.send(JSON.stringify(response));
+    // for (const message of messages) {
+    //   ws.send(message);
     // }
+  }
+  // snip
 
-    sendTextarea(data);
+// frontend/websocket.js
+  socket.onmessage = (event) => {
+    const message = JSON.parse(event.data);
+    messages.push(message);
 
-    // Broadcast to everyone else.
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-        sendTextarea(data);
-      }
-    });
-  });
+    console.log('====================================');
+    console.log('message', message);
+    console.log('messages', messages);
+
+    if (message.type === 'SESSION_ID') {
+      feSessionID = message.data.sessionID;
+      console.log(`feSessionID = ${feSessionID}`);
+      urlChange();
+    }
+
+    // Print message value to all textarea boxes in session
+    document.getElementById('mainTextArea').value = messages[0];
+    resizeTextBox();
+    getLines();
+    checkTime();
+  };
 });
